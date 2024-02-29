843b05a52c29d75d740e3a4a0ea060274a76459d
mazzinno <abdennasr3@gmail.com>
2024-02-29 02:41:44 +0100

    test

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# Using Puppet Installing Nginx server.
+
+package { 'nginx':
+  ensure => 'installed'
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+  content => 'Hello World!',
+}
+
+file_line { 'redirection-301':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
