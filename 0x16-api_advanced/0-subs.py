2360957ed3f70f18c9eb88405ed9eed64c434103
mazzinno <abdennasr3@gmail.com>
2024-04-13 01:26:51 +0000

    d

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""this Script will query the subscribers"""
+"""
+Contains the number_of_subscribers function
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
-    headers = {"User-Agent": "Mozilla/5.0"}
-
-    if not subreddit:
+    if subreddit is None or type(subreddit) is not str:
         return 0
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        return 0
-    return response.json()["data"]["subscribers"]
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
